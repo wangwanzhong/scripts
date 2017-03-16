e71da433b35201eaf895058e74827ec63f0e4921
wwz-PC\wwz <wangwanzhong@sphinxjoy.com>
2017-03-16 16:41:05 +0800

    add redis install script

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # scripts
+
+## redis
+
+- default version 3.2.8
+```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash```
+
+- install specific version
+```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash -s 3.2.8```
+
+
+## php
+
+```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash```
+
+## nginx
+
+```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash```
---
+++ ins_redis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Usage: ./ins-redis.sh 3.2.8
+
+set -e
+
+DEFAULT_VERSION="3.2.8"
+
+[ -z "$1" ] && VERSION=${DEFAULT_VERSION} || VERSION=$1
+
+echo "Install version ${VERSION}"
+
+yum -y install gcc-c++ make
+
+[ ! -f "redis-${VERSION}.tar.gz" ] && wget http://download.redis.io/releases/redis-${VERSION}.tar.gz || echo "use local package"
+
+tar zxf redis-${VERSION}.tar.gz
+cd redis-${VERSION}
+#(if can not pass: cd deps &&  make hiredis  jemalloc  linenoise  lua)
+make && make PREFIX=/opt/redis-${VERSION} install
+ln -s /opt/redis-${VERSION} /opt/redis
+mkdir -p /opt/redis/etc
+cp redis.conf /opt/redis/etc/
+ln -sf /opt/redis/etc/redis.conf /etc/redis.conf
+ln -sf /opt/redis/bin/* /usr/local/bin/
