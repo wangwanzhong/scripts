a26edf4ccc5863aabe24004503c01a8efc0e0354
wwz-PC\wwz <wangwanzhong@sphinxjoy.com>
2017-05-24 17:46:54 +0800

    bk 0524

--- README.md
+++ README.md
@@ -3,16 +3,38 @@
 ## redis
 
 - default version 3.2.8
-```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash```
+
+```
+# wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/redis/ins_redis.sh -O -| /bin/bash
+# vim /etc/redis.conf 
+# redis-server /etc/redis.conf
+```
 
 - install specific version
-```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash -s 3.2.8```
+
+```# wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/redis/ins_redis.sh -O -| /bin/bash -s 3.2.8```
 
 
 ## php
 
-```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash```
+```
+# wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/php/ins_php.sh -O -| /bin/bash
+(run script ins_extension.sh to install extension)
+(install extension pdo_mysql in php-5.6.18/ext/pdo_mysql/)
+# php-fpm -t
+# php-fpm
+```
 
 ## nginx
 
-```wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/ins_redis.sh -O -| /bin/bash```
+```
+# wget https://raw.githubusercontent.com/wangwanzhong/scripts/master/nginx/ins_nginx.sh -O -| /bin/bash
+# mkdir -p /dbdata/logs
+# mkdir -p /dbdata/web/default
+# mkdir -p /dbdata/web/vhosts
+# echo -e "<?php \n\tphpinfo();\n?>" > /dbdata/web/default/phpinfo.php 
+# chown -R www /dbdata/web/
+# vim /opt/nginx/conf/nginx.conf
+# nginx -t
+# nginx 
+```
--- php/ins_php.sh
+++ php/ins_php.sh
@@ -8,8 +8,8 @@ yum -y install epel-release
 yum -y install gcc-c++ make libxml2-devel curl-devel libjpeg-devel libpng-devel freetype-devel zlib-devel pcre-devel openssl-devel libxml2 libxml2-devel openssl openssl-devel libcurl libcurl-devel libjpeg libjpeg-devel freetype freetype-devel libc-client libc-client-devel libtool.x86_64 libtool-ltdl.x86_64 libtool-ltdl-devel.x86_64 openldap-devel libjpeg.x86_64 libpng.x86_64 freetype.x86_64 libjpeg-devel.x86_64 libpng-devel.x86_64 freetype-devel.x86_64 krb5* mcrypt mhash-devel libmcrypt-devel mysql-devel
 /usr/bin/id www || useradd -M www
 
-ln -s /usr/lib64 /usr/kerberos/lib
-ln -s /usr/lib64/libc-client.so /usr/lib/libc-client.so
+ln -sf /usr/lib64 /usr/kerberos/lib
+ln -sf /usr/lib64/libc-client.so /usr/lib/libc-client.so
 
 
 
@@ -22,8 +22,7 @@ cd php-5.6.18
 ./configure --prefix=/opt/php-5.6.18 --with-config-file-path=/etc/php --with-gd --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib --with-imap --with-kerberos --with-imap-ssl --with-mhash --with-mcrypt --with-curl --with-openssl --with-gettext --with-iconv-dir --with-libxml-dir --with-fpm-user=www --with-fpm-group=www --with-mysql=mysqlnd --with-mysqli --enable-fpm --enable-inline-optimization --enable-mbstring --enable-gd-native-ttf --enable-mbregex --enable-soap --enable-opcache
 
 make && make install
-ln -s /opt/php-5.6.18/ /opt/php
+ln -sf /opt/php-5.6.18/ /opt/php
 cp php.ini-production /opt/php/etc/php.ini
-mkdir /etc/php
-ln -sf /opt/php/etc/* /etc/php/
+ln -sf /opt/php/etc /etc/php
 ln -sf /opt/php/bin/* /opt/php/sbin/* /usr/local/bin/
