2def31e6007a0db2850e078a384c8132ce87a0f3
wwz-PC\wwz <wangwanzhong@sphinxjoy.com>
2017-10-31 14:08:26 +0800

    change nginx

--- php/ins_php.sh
+++ php/ins_php.sh
@@ -9,7 +9,7 @@ DEFAULT_VERSION="5.6.18"
 
 yum -y install epel-release
 
-yum -y install gcc-c++ make libxml2-devel curl-devel libjpeg-devel libpng-devel freetype-devel zlib-devel pcre-devel openssl-devel libxml2 libxml2-devel openssl openssl-devel libcurl libcurl-devel libjpeg libjpeg-devel freetype freetype-devel libc-client libc-client-devel libtool.x86_64 libtool-ltdl.x86_64 libtool-ltdl-devel.x86_64 openldap-devel libjpeg.x86_64 libpng.x86_64 freetype.x86_64 libjpeg-devel.x86_64 libpng-devel.x86_64 freetype-devel.x86_64 krb5* mcrypt mhash-devel libmcrypt-devel mysql-devel
+yum -y install gcc-c++ make libxml2-devel curl-devel libjpeg-devel libpng-devel freetype-devel zlib-devel pcre-devel openssl-devel libxml2 libxml2-devel openssl openssl-devel libcurl libcurl-devel libjpeg libjpeg-devel freetype freetype-devel libc-client libc-client-devel libtool.x86_64 libtool-ltdl.x86_64 libtool-ltdl-devel.x86_64 openldap-devel libjpeg.x86_64 libtool libpng.x86_64 freetype.x86_64 libjpeg-devel.x86_64 libpng-devel.x86_64 freetype-devel.x86_64 krb5* mcrypt mhash-devel libmcrypt-devel mysql-devel
 /usr/bin/id www || useradd -M www
 
 ln -sf /usr/lib64 /usr/kerberos/lib
@@ -30,6 +30,6 @@ make && make install
 rm -rf /opt/php
 ln -sf /opt/php-${VERSION} /opt/php
 cp php.ini-production /opt/php/etc/php.ini
-[ -d "/etc/php" ] && rm -rf /etc/php
+rm -rf /etc/php
 ln -sf /opt/php/etc /etc/php
 ln -sf /opt/php/bin/* /opt/php/sbin/* /usr/local/bin/
